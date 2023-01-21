b0864bbd8590fca7fdbace60644d64f7a944ea3c
Vitaly Tsaplin <tsaplin@adobe.com>
2023-01-21 22:00:05 +0100

    feat: add command options

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Website Data Miner
 A service for extracting data from websites using simple expressions.
 
+## Use-cases
+- You want something simple and minimalistic.
+- You prefer a declarative approach to extracting data from websites.
+- You plan to have a post-processing step to aggregate or analyze the data.
+
 ## Usage
 - Checkout the code
 - Install dependencies using `npm install`
@@ -19,4 +24,55 @@ A service for extracting data from websites using simple expressions.
 }
 ```
 
-For more information see an example of a Jupiter notebook in the `examples` folder. 
+## Command-line options
+```bash
+Options:
+  --no-prompt        No prompt
+  --no-headless      Disable headless mode
+  -p, --port <port>  Server port
+  -h, --help         Show help
+```
+
+## Examples
+Request payload:
+```json
+{
+  "url":"http://www.apple.com",
+  "expressions":[
+    {
+      "select":[
+        "tagName",
+        "textContent",
+        "getBoundingClientRect"
+      ],
+      "from":"h1",
+      "as":"headings"
+    }
+  ]
+}
+```
+Response body:
+```json
+[
+  {
+    "headings": [
+      {
+        "tagName": "H1",
+        "textContent": "Apple",
+        "getBoundingClientRect": {
+          "x": 0,
+          "y": 0,
+          "width": 1,
+          "height": 1,
+          "top": 0,
+          "right": 1,
+          "bottom": 1,
+          "left": 0
+        }
+      }
+    ]
+  }
+]
+```
+
+For more information see an interactive example in the Jupiter notebook. 
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
-import puppeteer, {Page} from 'puppeteer';
-import {autoScroll} from './utils';
+import puppeteer from 'puppeteer';
+import {autoScroll} from './utils.js';
+import {Command} from 'commander';
+import inquirer from 'inquirer';
+
+const DEFAULT_PORT = 3000;
+const DEFAULT_HEADLESS = true;
 
 type Expression = {
   select: string[];
@@ -34,8 +39,8 @@ function evaluateExpressions() {
   };
 }
 
-async function handleQueryRequest(queryRequest: QueryRequest) {
-  const browser = await puppeteer.launch();
+async function handleQueryRequest(queryRequest: QueryRequest, headless: boolean) {
+  const browser = await puppeteer.launch({headless});
 
   const page = await browser.newPage();
   await page.setViewport({
@@ -51,7 +56,7 @@ async function handleQueryRequest(queryRequest: QueryRequest) {
     await autoScroll(page);
   }
 
-  console.log('Rendering page ', page.url());
+  console.log(`\nRendering page ${page.url()} in ${headless ? 'headless' : 'headed'} mode...`);
 
   const results = await Promise.all(queryRequest.expressions.map(async expression => {
     console.log('Evaluating expression: ', expression);
@@ -64,22 +69,61 @@ async function handleQueryRequest(queryRequest: QueryRequest) {
   return results;
 }
 
-const router = express.Router()
+function serve(port: number, headless: boolean) {
+  const router = express.Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Server is up and running!')
-})
+  router.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Server is up and running!')
+  })
 
-router.post('/query', async (req: Request, res: Response) => {
-  console.log('Received request');
-  res.status(200).json(await handleQueryRequest(req.body as QueryRequest))
-  console.log('Sent response');
-})
+  router.post('/query', async (req: Request, res: Response) => {
+    console.log('Received request');
+    res.status(200).json(await handleQueryRequest(req.body as QueryRequest, headless))
+    console.log('Sent response');
+  })
 
-const app = express();
-app.use(express.json());
-app.use('/', router)
+  const app = express();
+  app.use(express.json());
+  app.use('/', router)
 
-app.listen(3000, () => {
-  console.log(`Server started at ${3000}`)
-})
+  app.listen(port, () => {
+    console.log(`\nServer started at http://localhost:${port}...\n`);
+  })
+}
+
+function getQuestions() {
+  return [
+    {
+      type: 'input',
+      name: 'port',
+      message: 'What is the server port?',
+      default: DEFAULT_PORT,
+    },
+    {
+      type: 'confirm',
+      name: 'headless',
+      message: 'Do you want to run in headless mode?',
+      default: DEFAULT_HEADLESS,
+    }
+  ];
+}
+
+async function main() {
+  const args = new Command()
+    .helpOption('-h, --help', 'Show help')
+    .option('--no-prompt', 'No prompt')
+    .option('--no-headless', 'Disable headless mode')
+    .option('-p, --port <port>', 'Server port')
+    .parse().opts();
+
+  const options = args.prompt ? await inquirer.prompt(getQuestions()) : args;
+
+  const port = options.port ?? DEFAULT_PORT;
+  const headless = options.headless ?? DEFAULT_HEADLESS;
+
+  serve(port, headless);
+}
+
+main().catch((err) => {
+  console.error(err);
+});
